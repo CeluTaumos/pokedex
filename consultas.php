c0fe0f365b553d516fee76a383df44a020162c27
LudmiAbril <ludmila.pereyra543@gmail.com>
2023-09-15 18:49:29 -0300

    detalle pokemon

--- consultas.php
+++ consultas.php
@@ -1,3 +1,4 @@
+
 <?php
 include_once("conexion-bd.php");
 //BUSQUEDA POR NOMBRE
@@ -11,22 +12,30 @@ if (isset($_POST['busqueda'])) {
         while ($fila = $resultado->fetch_assoc()) {
             // Imprime los resultados
             echo "Resultado: " . "Numero: " . $fila["Numero"] . " - Nombre: " . $fila["Nombre"] . " - Tipo: <img src='" . $fila["Tipo"] . "' alt='tipo' width=30 height=24>" .
-                " - Imagen: <img class='mobile' src='" . $fila["Imagen"] . "' alt='tipo' width=100 height=100>" . "<button>Ver detalles</button>" ."<br>";
+                " - Imagen: <img class='mobile' src='" . $fila["Imagen"] . "' alt='tipo' width=100 height=100>" . 
+                "<form action='ver-detalle.php' method='POST'><button name='detalle' value='" . $fila["Numero"] . "'>Ver detalles</button></form>" . "<br>";
+        }
+    } else if ($resultado->num_rows == 1) {
+        while ($fila = $resultado->fetch_assoc()) {
+
+            echo "<section style='display: flex'> <img class='mobile' src='" . $fila["Imagen"] . "' alt='tipo' width=350 height=350>
+            <div> <h1>".$fila["Nombre"]."</h1>  <p>".$fila["Descripcion"]."</p> </div> </section>";
         }
     } else {
+
         echo "No se encontraron resultados.";
     }
 
 } else {
     mostrarTodaLaTablaPokedex($conexion);
 }
-$eliminar= "";
+$eliminar = "";
 
 //Como obtener el valor del id del input
-if(isset($_POST['eliminar'])){
+if (isset($_POST['eliminar'])) {
     $numero = $_POST['eliminar'];
-    $eliminar="DELETE FROM pokemon WHERE Numero LIKE ".$numero;
-    
+    $eliminar = "DELETE FROM pokemon WHERE Numero LIKE " . $numero;
+
     $conexion->query($eliminar);
 
 }
---
+++ ver-detalle.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=+, initial-scale=1.0">
+    <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
+    <title>Pokedex-inicio</title>
+</head>
+
+<body>
+    <header>
+        <!-- LOGO Y TÍTULO -->
+        <nav class="navbar color-base">
+            <div class="container-fluid">
+                <a class="navbar-brand " href="index.php">
+                    <img src="./images/logo.png" alt="Logo" width="36" height="30" class=" d-inline-block align-text-top ">
+
+                </a>
+                <h4 class="logo">Pokedex:Gotta catch 'em all! </h4>
+
+
+        </nav>
+
+    </header>
+    <main class="container">
+    <div class="mt-3">
+            <form class="d-flex" role="search" method="POST" action="index-logueado.php">
+                <input name="busqueda" class="form-control me-2" type="search" placeholder="Ingrese el nombre, tipo o número de Pókemon" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">¿Quién es ese Pókemon?</button>
+            </form>
+            
+        </div>
+
+<?php
+include_once("conexion-bd.php");
+//Como obtener el valor del id del input
+if(isset($_POST['detalle'])){
+    $numero = $_POST['detalle'];
+    $res ="SELECT * FROM pokemon WHERE Numero LIKE " . $numero;
+    $pokemon_detalle =  $conexion->query($res);
+
+    while ($fila = $pokemon_detalle->fetch_assoc()) {
+
+        echo "<section style='display: flex'> <img class='mobile' src='" . $fila["Imagen"] . "' alt='tipo' width=350 height=350>
+        <div> <h1>".$fila["Nombre"]."</h1>  <p>".$fila["Descripcion"]."</p> </div> </section>";
+    }
+}
+
+?>
+
+</main>
+<footer></footer>
+
+    <!-- BOOSTRAP -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
