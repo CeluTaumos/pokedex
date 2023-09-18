8e48479f1ecd19e422964963424001b70af12030
Axel Daniel <axeelleguero@gmail.com>
2023-09-18 14:34:03 -0300

    Arreglado consulta

--- consultas.php
+++ consultas.php
@@ -7,13 +7,22 @@ if (isset($_POST['busqueda'])) {
     $sql = "SELECT * FROM pokemon WHERE Nombre LIKE '%$busqueda%'OR Tipo LIKE '%$busqueda%' OR Numero LIKE '$busqueda'";
     $resultado = $conexion->query($sql);
 
-    if ($resultado->num_rows > 0) {
+    if ($resultado->num_rows > 1) {
         while ($fila = $resultado->fetch_assoc()) {
             // Imprime los resultados
             echo "<div class='border border-3 border-primary'>" ." Resultado: " . "Numero: " . $fila["Numero"] . " - Nombre: " . $fila["Nombre"] . " - Tipo: <img src='" . $fila["Tipo"] . "' alt='tipo' width=30 height=24>" .
-                " - Imagen: <img class='mobile' src='" . $fila["Imagen"] . "' alt='tipo' width=100 height=100> " . "<br>" ."</div>";
+            " - Imagen: <img class='mobile' src='" . $fila["Imagen"] . "' alt='tipo' width=100 height=100>" . 
+            "<form action='ver-detalle.php' method='POST'><button name='detalle' value='" . $fila["Numero"] . "'>Ver detalles</button></form>" . "<br>";
         }
-    } else {
+    }
+    else if ($resultado->num_rows == 1) {
+        while ($fila = $resultado->fetch_assoc()) {
+
+            echo "<section style='display: flex'> <img class='mobile' src='" . $fila["Imagen"] . "' alt='tipo' width=350 height=350>
+            <div> <h1>".$fila["Nombre"]."</h1>  <p>".$fila["Descripcion"]."</p> </div> </section>";
+        }
+    }
+    else {
         echo "No se encontraron resultados.";
     }
 
